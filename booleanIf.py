b24375fc0f3eb5bc828e86f61ac7737ecbd1862a
bnokes8 <brady.nokes@outlook.com>
2020-09-26 13:48:37 -0600

    Added boolean and if statemnts code

---
+++ booleanIf.py
@@ -0,0 +1,83 @@
+## Brady Nokes 9/24/20 logical expressions and IF statements
+##
+## Boolean Value
+## Must have either True or False and they must be capitalized
+##
+##boolvar = True
+##isAwake = True
+##isInClass = True
+##
+## when you print it out it is not a string it will be a boolean
+##
+##print(type(boolvar))
+##print(isAwake)
+##print(isInClass)
+##
+## Logical Expressions, Either greater than or less than.
+##
+##logicExp = 12==5
+##print(logicExp)
+## when you print out that logicExp it will come out either as true or false
+## you can add an equals sign to the end of the less than or greater than to check to see if it is equal/
+## if you want to see if it will not be equal to it you have to do a !=
+## when you want to see if the number is equal to something then you have to do == two equal signs
+## you need to know all of the operators.
+##> greter than
+##< less than
+##>= less than or equal to
+##>= greater that or equal to
+##!= does not equal to 
+##== equals to
+
+##input1 = input("Enter a Number: ")
+##input2 = input("Enter a Number: ")
+##
+## the block of code after your expression will only execute if the expressin is true
+##
+##if input1 > input2 :
+##    print("1st number was bigger than the second number")
+
+
+password = "Password1!"
+
+
+userPass = input("Password Please: ")
+
+if userPass == password :
+    print("You Shall Pass")
+
+elif password != userPass :
+    print("You Shall Not Pass!")
+
+elif not password :
+
+else:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
