89a936fc38c01a824d938b4e05ace2e6d261b4af
bnokes8 <brady.nokes@outlook.com>
2020-10-01 14:04:41 -0600

    Updated num game

---
+++ compound.py
@@ -0,0 +1,5 @@
+playerScore = 100
+age = 19
+
+if ((playerScore > 100) and (age < 13 or age > 18)) :
+    print("test")
--- randomNumGuess.py
+++ randomNumGuess.py
@@ -1,21 +1,56 @@
 ## Brady Nokes 9/29/20
 # Guess My Number 1.0
 
-# picks a random number
+# imports random number library
 import random
 
-theNum = random.randint(1,10)
-##print(theNum )
-
+# intro
 print("\tWelcome to 'Guess my number'!")
-print("\nI'm Thinking of a number between 1 and 10.")
-print("Try to guess it in 3 attempts. ")
 
+question = input("What difficulty would you like, Easy, Medium, Or Hard?: ")
+if  ("Ea" in question) or ("ea" in question) or (question == "Easy") :
+    dif = 1
+    maxRange = 10
+    tries = 3
+elif ("M" in question) or ("m" in question):
+    dif = 2
+    maxRange = 50
+    tries = 5
+else :
+    dif = 3
+    maxRange = 100
+    tries = 10
+
+
+
+theNum = random.randint(1,maxRange)
+print(theNum )
+
+
+print("\nI'm Thinking of a number between 1 and "+str(maxRange)+".")
+print("Try to guess it in "+str(tries)+" attempts. ")
+
+# boolean win condition
 winner = False
 
-guess = int(input("Pick a Number between 1 and 10: "))
+# 1st guess
+guess = int(input("Pick a Number between 1 and "+str(maxRange)+": "))
 
+
+if guess == theNum :
+    winner = True
+
+elif guess < theNum :
+    print("Guess Higher")
+
+else :
+    print("Guess Lower")
+
+
+
+# guess 2
 if winner == False :
+    guess = int(input("Pick a Number between 1 and "+str(maxRange)+": "))
     if guess == theNum :
         winner = True
 
@@ -27,9 +62,94 @@ if winner == False :
 
 
 
-
+#guess 3
 if winner == False :
-    guess = int(input("Pick a Number between 1 and 10: "))
+    if dif > 1:
+        guess = int(input("Pick a Number between 1 and "+str(maxRange)+": "))
+        if guess == theNum :
+            winner = True
+
+        elif guess < theNum :
+            pass
+
+        else :
+            pass
+# guess 4
+if winner == False :
+    if dif > 1:
+        guess = int(input("Pick a Number between 1 and "+str(maxRange)+": "))
+        if guess == theNum :
+            winner = True
+
+        elif guess < theNum :
+            print("Guess Higher")
+
+        else :
+            print("Guess Lower")
+### guess 5
+if winner == False :
+    if dif > 2:
+        guess = int(input("Pick a Number between 1 and "+str(maxRange)+": "))
+        if guess == theNum :
+            winner = True
+
+        elif guess < theNum :
+            print("Guess Higher")
+
+        else :
+            print("Guess Lower")
+# guess 6
+if winner == False :
+    if dif > 2:
+        guess = int(input("Pick a Number between 1 and "+str(maxRange)+": "))
+        if guess == theNum :
+            winner = True
+
+        elif guess < theNum :
+            print("Guess Higher")
+
+        else :
+            print("Guess Lower")
+# guess 7
+if winner == False :
+    if dif > 2:
+        guess = int(input("Pick a Number between 1 and "+str(maxRange)+": "))
+        if guess == theNum :
+            winner = True
+
+        elif guess < theNum :
+            print("Guess Higher")
+
+        else :
+            print("Guess Lower")
+# guess 8
+if winner == False :
+    if dif > 2:
+        guess = int(input("Pick a Number between 1 and "+str(maxRange)+": "))
+        if guess == theNum :
+            winner = True
+
+        elif guess < theNum :
+            print("Guess Higher")
+
+        else :
+            print("Guess Lower")
+# guess 9
+if winner == False :
+    if dif > 2:
+        guess = int(input("Pick a Number between 1 and "+str(maxRange)+": "))
+        if guess == theNum :
+            winner = True
+
+        elif guess < theNum :
+            print("Guess Higher")
+
+        else :
+            print("Guess Lower")
+
+#last guess
+if winner == False :
+    guess = int(input("Pick a Number between 1 and "+str(maxRange)+": "))
     if guess == theNum :
         winner = True
 
@@ -39,21 +159,7 @@ if winner == False :
     else :
         print("Guess Lower")
 
-
-
-
-if winner == False :
-    guess = int(input("Pick a Number between 1 and 10: "))
-    if guess == theNum :
-        winner = True
-
-    elif guess < theNum :
-        pass
-
-    else :
-        pass
-
-
+# end condition
 if winner == True :
     print("You Win")
 else :
