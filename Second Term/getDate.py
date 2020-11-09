ff3465dc4f6da2dfddb6a870740a7a9464f8dec0
Brady Nokes <brady.nokes@tooeleschools.org>
2020-11-09 14:24:10 -0700

    updated oregon trail

---
+++ Second Term/getDate.py
@@ -0,0 +1,15 @@
+import datetime
+
+def get_day_of_the_week(target):
+     try:
+         thisDate = datetime.datetime.strptime(target,"%Y-%m-%d")
+         dayOfWeek = thisDate.strftime("%A")
+         return dayOfWeek
+     except:
+         "invalid YYYY-MM-DD date"
+
+userInput = input("What is the date you want? YYYY-MM-DD ")
+print(get_day_of_the_week(userInput))
+
+
+
--- oregonTrail/oregonTrail.py
+++ oregonTrail/oregonTrail.py
@@ -2,11 +2,11 @@
 # 11/3/20
 # title screen that shows creators and copyright
 
-# Function for the main menu. will ask the user what they want to do. 
+# Function for the main menu. will ask the user what they want to do.
 def menu_options(options):
     index = 1
     for i in options:
-        print(str.format("{}.......{}",index,i))
+        print(str.format("{}){}",index,i))
         index+=1
     while True:
 
@@ -33,7 +33,7 @@ def title_screen():
 #     # #     # #       #       #     # #  #  # #     # 
  #   #  #     # #       #       ####### #   # # #     # 
   # #   #     # #       #     # #     # #    ## #     # 
-   #    ####### #######  #####  #     # #     # #######''')
+   #    ####### #######  #####  #     # #     # #######\n''')
   while True:
     userChoice = ["Travel the trail","Learn about the trail", "Quit"]
     choice = menu_options(userChoice)
@@ -55,19 +55,19 @@ def learn():
 | |_| | | |  __/ (_| | (_) | | | |   | || | | (_| | | |
  \___/|_|  \___|\__, |\___/|_| |_|   |_||_|  \__,_|_|_|
                 |___/          ''',
-"Try taking a journey by covered wagon across 2000 miles of plains, rivers, and mountains. Try! On the plains, will you slosh your oxen through mud and water-filled ruts or will you plod through dust six inches deep?",
+"\nTry taking a journey by covered wagon across 2000 miles of plains, rivers, and mountains. Try! On the plains, will you slosh your oxen through mud and water-filled ruts or will you plod through dust six inches deep?",
 #second page
-'''How will you cross the rivers? If have money, you might take a ferry (if there is one).
+'''\nHow will you cross the rivers? If have money, you might take a ferry (if there is one).
 Or, you can ford the river and hope oyu and your wagon aren't swallowed alive!''',
 #third page
-'''What about supplies? Well, if you're low on food you can hunt.
+'''\nWhat about supplies? Well, if you're low on food you can hunt.
 You might get a buffalo...
 You might. And there are bears in the mountains.''',
 #fourth page
-'''At the Dalles, you can try navigating the Columbia River, but if running the rapids with a makeshift raft makes you queasy,
+'''\nAt the Dalles, you can try navigating the Columbia River, but if running the rapids with a makeshift raft makes you queasy,
 better take the Barlow Road.''',
 #fifth page
-'''If for some reason you don't survive -- your wagon burns, or thieves steal your oxen, or you run out of provisions, die of Covid-19,
+'''\nIf for some reason you don't survive -- your wagon burns, or thieves steal your oxen, or you run out of provisions, die of Covid-19,
 don't give up! Try again... and again... and again utnil your name is up there with the pro's.''',
 #sixth page (credits)
 '''This game was re-made by:
@@ -80,14 +80,25 @@ don't give up! Try again... and again... and again utnil your name is up there w
 
 
 
-  
+def family_Setup(question):
+  wagonLeader = get_good_name("What is your name?")
+  fam_list = []
+  low = 2
+  high = 7
+  fam_members = get_good_number(question,low,high)
+  for i in range(fam_members):
+      name = get_good_name("What is their name?: ")
+      fam_list.append(name)
+  return fam_list, wagonLeader
 
 def play():
   money = 0
   prof = ""
-  listofprofs = ["Banker","Carpenter","Farmer","Learn the difference"]
-  money,prof = char_creator(listofprofs)
+  listOfProfs = ["Banker","Carpenter","Farmer","Learn the difference"]
+  money,prof = char_creator(listOfProfs)
   print(money, prof)
+  family_Setup("How many family members do you have?")
+  
 
 
 def char_creator(options):
@@ -120,6 +131,27 @@ banker earns the least.\n''')
     input("Press enter to go back to pick your profession")
 
   return money, prof
+def get_good_name(question):
+    while True:
+        name = input(question)
+        if name.isnumeric and len(name) <=1 :
+            print("Invalid Name")
+        else:
+            return name
+            break
+
+# NEED TO FINISH FIXING THE ERRORS
+def get_good_number(question,low,high):
+    while True:
+        number = input(question)
+        if number.isnumeric() :
+            number = int(number)
+            if number >=1 and number <= 8:
+                return number
+        print ("not a good number")
+
+
+
 
 
 
