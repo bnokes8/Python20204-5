4cab39d3b62dcce27a6336b5d78f18b23ea88393
bnokes8 <brady.nokes@outlook.com>
2021-03-23 15:08:24 -0600

    shmup

--- shmup2/main.py
+++ shmup2/main.py
@@ -17,6 +17,8 @@ from os import *
 class Player(pg.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
+        self.shield = 100
+        self.fuel = 100
         # self.image = pg.Surface((50, 40))
         # self.image.fill(GREEN)
         self.image = player_img
@@ -29,21 +31,29 @@ class Player(pg.sprite.Sprite):
         self.rect.centerx = (WIDTH / 2)
         self.rect.bottom = (HEIGHT - (HEIGHT * .05))
         self.speedx = 0
+        self.shoot_delay = 250
+        self.last_shot = pg.time.get_ticks()
 
     def update(self):
         self.speedx = 0
         self.speedy = 0
         keystate = pg.key.get_pressed()
-        if keystate[pg.K_LEFT] or keystate[pg.K_a]:
+        if (keystate[pg.K_LEFT] or keystate[pg.K_a]) and self.fuel > 0:
             self.speedx = -5
-        if keystate[pg.K_RIGHT] or keystate[pg.K_d]:
+            self.fuel -= .025
+        if (keystate[pg.K_RIGHT] or keystate[pg.K_d]) and self.fuel > 0:
             self.speedx = 5
-        if keystate[pg.K_DOWN] or keystate[pg.K_s]:
-            self.speedy = 5
-        if keystate[pg.K_UP] or keystate[pg.K_w]:
-            self.speedy = -5
-        if keystate[pg.K_KP5]:
-            self.rect.center = (WIDTH/2,HEIGHT/2)
+            self.fuel -= .025
+        if keystate[pg.K_SPACE]:
+            player.shoot()
+        # if keystate[pg.K_DOWN] or keystate[pg.K_s]:
+        #     self.speedy = 5
+        #     self.fuel -= 2
+        # if keystate[pg.K_UP] or keystate[pg.K_w]:
+        #     self.speedy = -5
+        #     self.fuel -= 2
+        # if keystate[pg.K_KP5]:
+        #     self.rect.center = (WIDTH/2,HEIGHT/2)
         # Player Shooting
         # if keystate[pg.K_SPACE]:
         #     self.shoot()
@@ -59,9 +69,13 @@ class Player(pg.sprite.Sprite):
             self.rect.bottom = HEIGHT
 
     def shoot(self):
-        b = Bullet(self.rect.centerx,self.rect.top - 1)
-        all_sprites.add(b)
-        bullet_group.add(b)
+        now = pg.time.get_ticks()
+        if now - self.last_shot > self.shoot_delay:
+            self.last_shot = now
+            b = Bullet(self.rect.centerx,self.rect.top - 1)
+            all_sprites.add(b)
+            bullet_group.add(b)
+            #shoot_sound.play()
 
 class Bullet(pg.sprite.Sprite):
     def __init__(self,x,y):
@@ -87,6 +101,32 @@ class Bullet(pg.sprite.Sprite):
 
 
 
+# EXPLOSIONS
+class Explosions(pg.sprite.Sprite):
+
+    def __init__(self, center, size):
+        super(Explosions, self).__init__()
+        self.size = size
+        self.image = exp_anim[self.size][0]
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+        self.frame = 0
+        self.last_update = pg.time.get_ticks()
+        self.frame_rate = 50
+
+
+    def update(self):
+        now = pg.time.get_ticks()
+        if now - self.last_update > self.frame_rate:
+            self.last_update = now
+            self.frame += 1
+            if self.frame - len(exp_anim[self.size]):
+                self.kill()
+            else:
+                center = self.rect.center
+                self.image = exp_anim[self.size][self.frame]
+                self.rect = self.image.get_rect()
+                self.rect.center = center
 
 class NPC(pg.sprite.Sprite):
     def __init__(self):
@@ -131,6 +171,7 @@ class NPC(pg.sprite.Sprite):
 HEIGHT = 900
 WIDTH = 600
 FPS = 60
+font_name = pg.font.match_font('arial')
 
 # Colors (R,G,B)
 BLACK = (0, 0, 0)
@@ -150,6 +191,7 @@ scores_folder = path.join(game_folder,"scores")
 background_folder = path.join(imgs_folder,"backgrounds")
 player_folder = path.join(imgs_folder,"player")
 enemy_img_folder = path.join(imgs_folder,"enemies")
+animation_folder = path.join(imgs_folder, "animations")
 #
 
 ####################################################################
@@ -159,6 +201,19 @@ def spawn_npc():
     npc = NPC()
     npc_group.add(npc)
     all_sprites.add(npc)
+
+
+def draw_bar(surf,x,y,pct,color):
+    if pct < 0:
+        pct = 0
+    b_len = 180
+    b_height = 40
+    fill = (pct/100*b_len)
+    outline = pg.Rect(x,y,b_len,b_height)
+    fill_rect = pg.Rect(x,y,fill,b_height)
+    pg.draw.rect(surf,color,fill_rect)
+    pg.draw.rect(surf, WHITE, outline, 3)
+
 
 # initialize pygame and create window
 ####################################################################
@@ -178,8 +233,28 @@ npc_img = pg.image.load(path.join(enemy_img_folder,"meteormedium.png")).convert(
 player_img = pg.image.load(path.join(player_folder,"playergreen.png")).convert()
 bullet_img = pg.image.load(path.join(player_folder,"bulletred.png")).convert()
 
+
+
+exp_anim = {}
+exp_anim["lg"] = []
+exp_anim["sm"] = []
+for i in range(9):
+    filename = "regularExplosion0{}.png".format(i)
+    img = pg.image.load(path.join(animation_folder,filename)).convert()
+    img.set_colorkey(BLACK)
+    img_lg = pg.transform.scale(img, (100, 1000))
+    exp_anim["lg"].append(img_lg)
+    img_sm = pg.transform.scale(img, (75, 75))
+    exp_anim["sm"].append(img_lg)
+
+
+
+
+####################################################################
+# Load Snds
 ####################################################################
 
+####################################################################
 # create Sprite groups
 ####################################################################
 all_sprites = pg.sprite.Group()
@@ -254,11 +329,24 @@ while playing:
 
     hits = pg.sprite.spritecollide(player,npc_group,True,pg.sprite.collide_circle)
     if hits:
+        exp = Explosions(hits[0].rect.center,"sm")
+        all_sprites.add(exp)
+        #r.choice(expl_snds).play()
         spawn_npc()
+        player.shield -= hits[0].radius*2
+        print(player.shield)
+        if player.shield >= 0:
+            exp = Explosions(player.rect.center,"lg")
+            all_sprites.add(exp)
+            player.kill()
+            playing = False
+
         #playing = False
     # Bullet Hits NPC
     hits = pg.sprite.groupcollide(npc_group, bullet_group, True, True,pg.sprite.collide_circle)
     for hit in hits:
+        exp = Explosions(hit.rect.center,"sm")
+        all_sprites.add(exp)
         spawn_npc()
 
     all_sprites.update()
@@ -268,6 +356,11 @@ while playing:
     screen.fill(BLACK)
     screen.blit(background,bg_rect)
     all_sprites.draw(screen)
+    # Draw HUD
+
+    draw_bar(screen, 5, 15, player.shield, GREEN)
+
+    draw_bar(screen, 5, 55, player.fuel, BLUE)
 
     pg.display.flip()
     ##################################################
